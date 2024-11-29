95a0af1210c5a4313363d3c329e7de11ab3a2723
Faith Chepkoech Birgen <faybirgen815@gmail.com>
2024-11-29 14:21:03 +0300

    Fix issues in index.js, update dependencies in package.json, and update results.json

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+// WELCOME MESSAGE
+const currentUser = 'Grace Hopper';
+// const welcomeMessage = 'Welcome to Flatbook';
+// const  welcomeMessage = currentUser;
+// const welcomeMessage = 'Welcome to Flatbook, currentUser';
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// EXCITED MESSAGE
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// SHORT GREETING
+// const shortGreeting = 'Welcome,'
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
